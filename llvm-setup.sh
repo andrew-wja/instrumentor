3def92830f55937b28df612ffcfee1c5c384ed8b
Andrew <aanderso@tcd.ie>
2021-04-08 14:36:01 +0100

    Add llvm setup

---
+++ llvm-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mkdir -p llvm-build
+
+pushd llvm-build
+
+cmake -G "Unix Makefiles" ../llvm-project/llvm/ -DCMAKE_{C_COMPILER=clang,CXX_COMPILER=clang++,LINKER=ld.lld,RANLIB=$(which llvm-ranlib),AR=$(which llvm-ar),BUILD_TYPE=Release,INSTALL_PREFIX=$(realpath ../llvm-root)} \
+-DLLVM_{TARGETS_TO_BUILD="X86",ENABLE_PROJECTS="clang;compiler-rt;openmp",INCLUDE_{TOOLS=ON,EXAMPLES=OFF,TESTS=OFF,BENCHMARKS=OFF},ENABLE_BINDINGS=OFF} \
+-DBUILD_SHARED_LIBS=ON -DCOMPILER_RT_BUILD_{XRAY=OFF,LIBFUZZER=OFF,PROFILE=OFF,MEMPROF=OFF}
+
+cmake --build . --parallel
+
+cmake --build . --target install
+
+popd
