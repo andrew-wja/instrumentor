0285493c645c46178894c930c6ec98a6138173ed
Andrew <aanderso@tcd.ie>
2021-04-09 09:16:24 +0100

    Update with LLVM module loading and saving code

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,31 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (parseIR, printIR, parseBC, printBC) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Prelude hiding (readFile, print)
+import qualified LLVM.AST as AST
+import LLVM.Internal.Context (withContext)
+import LLVM.Internal.Module (withModuleFromLLVMAssembly, moduleLLVMAssembly, withModuleFromBitcode, moduleBitcode, withModuleFromAST, moduleAST, Module(..), File(..))
+import Data.ByteString (readFile, ByteString)
+
+parseIR :: String -> IO AST.Module
+parseIR file = do
+  fcts <- readFile file
+  withContext (\ctx -> do
+    withModuleFromLLVMAssembly ctx fcts toAST)
+
+parseBC :: String -> IO AST.Module
+parseBC file = do
+  withContext (\ctx -> do
+    withModuleFromBitcode ctx (File file) toAST)
+
+printIR :: AST.Module -> IO ByteString
+printIR m = do
+  withContext $ (\ctx -> do
+    withModuleFromAST ctx m moduleLLVMAssembly)
+
+printBC :: AST.Module -> IO ByteString
+printBC m = do
+  withContext $ (\ctx -> do
+    withModuleFromAST ctx m moduleBitcode)
+
+toAST :: LLVM.Internal.Module.Module -> IO AST.Module
+toAST = moduleAST
