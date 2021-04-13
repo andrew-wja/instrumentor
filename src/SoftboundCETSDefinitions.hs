189fddd139f29c052758d674b4d13d2697ebb51e
Andrew <aanderso@tcd.ie>
2021-04-13 10:07:43 +0100

    Add test code

--- src/SoftboundCETSDefinitions.hs
+++ src/SoftboundCETSDefinitions.hs
@@ -15,24 +15,24 @@ import Data.Set hiding (map)
 sbcetsModule :: ModuleBuilder ()
 sbcetsModule = do
   _ <- extern (mkName "__softboundcets_spatial_load_dereference_check")
-         [ptr void, ptr void, ptr void, i64] void
+         [ptr i8, ptr i8, ptr i8, i64] void
 
   _ <- extern (mkName "__softboundcets_spatial_store_dereference_check")
-         [ptr void, ptr void, ptr void, i64] void
+         [ptr i8, ptr i8, ptr i8, i64] void
 
   _ <- extern (mkName "__softboundcets_temporal_load_dereference_check")
-         [ptr void, i64, ptr void, ptr void] void
+         [ptr i8, i64, ptr i8, ptr i8] void
 
   _ <- extern (mkName "__softboundcets_temporal_store_dereference_check")
-         [ptr void, i64, ptr void, ptr void] void
+         [ptr i8, i64, ptr i8, ptr i8] void
 
-  _ <- extern (mkName "__softboundcets_get_global_lock") [] (ptr void)
+  _ <- extern (mkName "__softboundcets_get_global_lock") [] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_introspect_metadata")
-         [ptr void, ptr void, ptr void, i64] void
+         [ptr i8, ptr i8, ptr i8, i64] void
 
   _ <- extern (mkName "__softboundcets_copy_metadata")
-         [ptr void, ptr void, i64] void
+         [ptr i8, ptr i8, i64] void
 
   _ <- extern (mkName "__softboundcets_allocate_shadow_stack_space")
          [i32] void
@@ -40,71 +40,71 @@ sbcetsModule = do
   _ <- extern (mkName "__softboundcets_deallocate_shadow_stack_space") [] void
 
   _ <- extern (mkName "__softboundcets_store_base_shadow_stack")
-         [ptr void, i32] void
+         [ptr i8, i32] void
 
   _ <- extern (mkName "__softboundcets_store_bound_shadow_stack")
-         [ptr void, i32] void
+         [ptr i8, i32] void
 
   _ <- extern (mkName "__softboundcets_load_base_shadow_stack")
-         [i32] (ptr void)
+         [i32] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_load_bound_shadow_stack")
-         [i32] (ptr void)
+         [i32] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_load_key_shadow_stack")
          [i32] i64
 
   _ <- extern (mkName "__softboundcets_load_lock_shadow_stack")
-         [i32] (ptr void)
+         [i32] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_store_key_shadow_stack")
          [i64, i32] void
 
   _ <- extern (mkName "__softboundcets_store_lock_shadow_stack")
-         [ptr void, i32] void
+         [ptr i8, i32] void
 
   _ <- extern (mkName "__softboundcets_stack_memory_allocation")
-         [ptr void, i64] void
+         [ptr i8, i64] void
 
   _ <- extern (mkName "__softboundcets_stack_memory_deallocation")
          [i64] void
 
   _ <- extern (mkName "__softboundcets_metadata_map")
-         [ptr void] (ptr void)
+         [ptr i8] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_metadata_load_base")
-         [ptr void] (ptr void)
+         [ptr i8] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_metadata_load_bound")
-         [ptr void] (ptr void)
+         [ptr i8] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_metadata_load_key")
-         [ptr void] i64
+         [ptr i8] i64
 
   _ <- extern (mkName "__softboundcets_metadata_load_lock")
-         [ptr void] (ptr void)
+         [ptr i8] (ptr i8)
 
   _ <- extern (mkName "__softboundcets_metadata_load_vector")
-         [ptr void, ptr void, ptr void, i64, ptr void, i32] void
+         [ptr i8, ptr i8, ptr i8, i64, ptr i8, i32] void
 
   _ <- extern (mkName "__softboundcets_metadata_store_vector")
-         [ptr void, ptr void, ptr void, i64, ptr void, i32] void
+         [ptr i8, ptr i8, ptr i8, i64, ptr i8, i32] void
 
   _ <- extern (mkName "__softboundcets_metadata_load")
-         [ptr void, ptr void, ptr void, ptr i64, ptr void] void
+         [ptr i8, ptr i8, ptr i8, ptr i64, ptr i8] void
 
   _ <- extern (mkName "__softboundcets_metadata_store")
-         [ptr void, ptr void, ptr void, i64, ptr void] void
+         [ptr i8, ptr i8, ptr i8, i64, ptr i8] void
 
   _ <- extern (mkName "__softboundcets_spatial_call_dereference_check")
-         [ptr void, ptr void, ptr void] void
+         [ptr i8, ptr i8, ptr i8] void
 
   _ <- extern (mkName "__softboundcets_memcopy_check")
-         [ptr void, ptr void, i64, ptr void, ptr void, ptr void, ptr void, i64,
-          ptr void, i64, ptr void] void
+         [ptr i8, ptr i8, i64, ptr i8, ptr i8, ptr i8, ptr i8, i64,
+          ptr i8, i64, ptr i8] void
 
   _ <- extern (mkName "__softboundcets_memset_check")
-         [ptr void, i64, ptr void, ptr void, i64, ptr void] void
+         [ptr i8, i64, ptr i8, ptr i8, i64, ptr i8] void
 
   return ()
 
