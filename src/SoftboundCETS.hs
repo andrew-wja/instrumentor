7664f5d40736bc5ce2941ffa105f38a2b6b7766c
Andrew <aanderso@tcd.ie>
2021-04-13 11:04:39 +0100

    Set up the complete harness for transforming functions

--- src/SoftboundCETS.hs
+++ src/SoftboundCETS.hs
@@ -5,29 +5,79 @@ import Data.String (IsString(..))
 import LLVM.AST
 import LLVM.AST.Global
 import LLVM.IRBuilder.Module
+import LLVM.IRBuilder.Monad
+import LLVM.IRBuilder.Internal.SnocList
 import SoftboundCETSDefinitions
 
 instrument :: Module -> IO Module
 instrument m = do
   let sbcModule = buildModule (fromString "softboundcets") sbcetsModule
-  putStrLn $ show $ functionsToInstrument m
   return $ sbcModule { moduleName = moduleName m,
                        moduleSourceFileName = moduleSourceFileName m,
                        moduleDataLayout = moduleDataLayout m,
                        moduleTargetTriple = moduleTargetTriple m,
                        moduleDefinitions = moduleDefinitions sbcModule ++
-                                           moduleDefinitions m }
+                                           (doInstrumentation $ moduleDefinitions m) }
   where
-    functionsToInstrument :: Module -> Set Name
-    functionsToInstrument m' = union (singleton $ mkName "main")
-                                     (difference (fromList $ map getFuncName
-                                                           $ filter isFuncDef
-                                                           $ moduleDefinitions m')
-                                                 (union ignoredFunctions
-                                                        wrappedFunctions))
+    doInstrumentation :: [Definition] -> [Definition]
+    doInstrumentation defs =
+      let ifs = functionsToInstrument defs
+      in map (instrumentFunction ifs) defs
+
+    functionsToInstrument :: [Definition] -> Set Name
+    functionsToInstrument defs = union (singleton $ mkName "main")
+                                       (difference (fromList $ map getFuncName
+                                                             $ filter isFuncDef
+                                                             $ defs)
+                                                   (union ignoredFunctions
+                                                          wrappedFunctions))
 
     isFuncDef (GlobalDefinition (Function {})) = True
     isFuncDef _ = False
 
     getFuncName (GlobalDefinition f@(Function {})) = name f
     getFuncName _ = undefined
+
+    instrumentFunction ifs g@(GlobalDefinition f@(Function {})) =
+      if member (name f) ifs then
+        GlobalDefinition $ f { basicBlocks = execIRBuilder emptyIRBuilder $ instrumentBlocks $ basicBlocks f }
+      else g
+
+    instrumentFunction _ x = x
+
+    instrumentBlocks blocks = do
+      mapM emitBlock blocks
+
+    emitBlock (BasicBlock n i t) = do
+      emitBlockStart n
+      mapM_ emitNamedInst i
+      emitNamedTerm t
+
+    emitNamedInst (n := i) = do
+      modifyBlock $ \bb -> bb
+        { partialBlockInstrs = partialBlockInstrs bb `snoc` (n := i) }
+
+    emitNamedInst (Do i) = do
+      emitInstrVoid i
+
+    emitNamedTerm (_ := _) = undefined -- we should never see this happen
+                                       -- named terminators are a quirk of LLVM IR
+
+    emitNamedTerm (Do t) = do
+      modifyBlock $ \bb -> bb
+        { partialBlockTerm = Just (Do t) }
+
+    -- ~ (BasicBlock n i t) =
+      -- ~ BasicBlock n (concatMap instrumentInst i) t
+
+    -- ~ instrumentInst :: Named Instruction -> [Named Instruction]
+    -- ~ instrumentInst (n := i)
+      -- ~ | (Load {}) <- i = execIRBuilder $ do
+        -- ~ call (ConstantOperand $ GlobalReference "__softboundcets_spatial_load_deference_check" ...
+        -- ~ call (ConstantOperand $ GlobalReference "__softboundcets_temporal_load_deference_check" ...
+
+      -- ~ | (Store {}) <- i =
+      -- ~ call (ConstantOperand $ GlobalReference "__softboundcets_spatial_store_deference_check" ...
+      -- ~ call (ConstantOperand $ GlobalReference "__softboundcets_temporal_store_deference_check" ...
+
+    -- ~ instrumentInst i = pure [i]
