5f377184f4785dfff0a8b0b504b8ade0dddf6237
Andrew <aanderso@tcd.ie>
2021-04-23 13:42:10 +0100

    Update LLVM 12 submodule and llvm-hs submodule. Work around GHC 8.10 bug by reverting to GHC 8.8.4

--- scripts/build-llvm.sh
+++ scripts/build-llvm.sh
@@ -4,11 +4,10 @@ mkdir -p llvm-build
 
 pushd llvm-build
 
-cmake -G "Unix Makefiles" ../llvm-project/llvm/ -DCMAKE_{C_COMPILER=clang,CXX_COMPILER=clang++,LINKER=ld.lld,RANLIB=$(which llvm-ranlib),AR=$(which llvm-ar),BUILD_TYPE=Release,INSTALL_PREFIX=$(realpath ../llvm-root)} \
--DLLVM_{TARGETS_TO_BUILD="X86",ENABLE_PROJECTS="clang;compiler-rt;openmp",INCLUDE_{TOOLS=ON,EXAMPLES=OFF,TESTS=OFF,BENCHMARKS=OFF},ENABLE_BINDINGS=OFF,PARALLEL_LINK_JOBS=1,BUILD_LLVM_DYLIB=ON,LINK_LLVM_DYLIB=ON} \
--DCOMPILER_RT_BUILD_{XRAY=OFF,LIBFUZZER=OFF,PROFILE=OFF,MEMPROF=OFF}
+cmake -Wno-dev -G "Ninja" ../llvm-project/llvm/ -DCMAKE_{C_COMPILER=clang,CXX_COMPILER=clang++,LINKER=ld.lld,RANLIB=$(which llvm-ranlib),AR=$(which llvm-ar),BUILD_TYPE=Release,INSTALL_PREFIX=$(realpath ../llvm-root)} \
+-DLLVM_{TARGETS_TO_BUILD="X86",ENABLE_PROJECTS="clang;",INCLUDE_{TOOLS=ON,EXAMPLES=OFF,TESTS=OFF,BENCHMARKS=OFF},ENABLE_BINDINGS=OFF,PARALLEL_LINK_JOBS=1,BUILD_LLVM_DYLIB=ON,LINK_LLVM_DYLIB=ON} \
 
-cmake --build . --parallel 2
+cmake --build . --parallel 32
 
 cmake --build . --target install
 
