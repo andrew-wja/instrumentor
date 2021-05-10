58d8a760922e797c961f97bdec8f52986a372f7c
Andrew <aanderso@tcd.ie>
2021-05-10 17:33:53 +0100

    Try again to get asan built in bundled llvm

--- scripts/build-runtimes.sh
+++ scripts/build-runtimes.sh
@@ -4,6 +4,8 @@ mkdir -p runtimes-build
 
 pushd runtimes-build
 
+rm -rf *
+
 cmake -G "Unix Makefiles" ../runtimes -DCMAKE_BUILD_TYPE=$1 -DLLVM_INSTALL_DIR=$(realpath ../llvm-root) -DCMAKE_C_COMPILER=$(realpath ../llvm-root/bin/clang) -DCMAKE_CXX_COMPILER=$(realpath ../llvm-root/bin/clang++)
 
 cmake --build .
